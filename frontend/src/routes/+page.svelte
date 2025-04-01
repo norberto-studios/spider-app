84a69e7a0789a7331fb00b1e00a842066445cf20
Norberto Fernandez <norbertostudios@gmail.com>
2025-04-01 04:02:52 -0400

    added frontend threlte for 3d

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,13 @@
+<script>
+    import { Canvas, T } from '@threlte/core'
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<Canvas>
+    <T.Mesh position.y={1}>
+        <T.BoxGeometry args={[1, 2, 1]} />
+        <T.MeshBasicMaterial color="hotpink" />
+    </T.Mesh>
+</Canvas>
